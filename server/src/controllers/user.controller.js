ca8a7accbc48975aac1de06a84de5b3d48cb5395
khushi979822 <khushikumari5114@gmail.com>
2026-07-07 19:34:46 +0530

    fix: streamline photo upload handling in EditUserProfile function

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -22,10 +22,10 @@ export const EditUserProfile = async (req, res, next) => {
     }
 
     if (newPhoto) {
+      existingUser?.photo?.publicId &&
+        (await cloudinary.uploader.destroy(existingUser.photo.publicId));
 
-     
-      
-        const b64 = Buffer.from(newPhoto.buffer).toString("base64");
+      const b64 = Buffer.from(newPhoto.buffer).toString("base64");
       const dataURI = `data:${newPhoto.mimetype};base64,${b64}`;
       // console.log(dataURI.slice(0, 100));
 
