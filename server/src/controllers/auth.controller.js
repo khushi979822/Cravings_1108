59317e6d481ea47f9fc0816951dfbcb10defcec3
khushi979822 <khushikumari5114@gmail.com>
2026-07-12 11:33:07 +0530

    feat: add OTP functionality for user registration and password reset

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,12 +1,25 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { genToken } from "../utils/auth.service.js";
+import OTP from "../models/otp.model.js";
+
+import { genOTPToken } from "../utils/auth.service.js";
+import { sendOTPEmail } from "../utils/email.service.js";
 
 export const RegisterUser = async (req, res, next) => {
   try {
-    const { fullName, email, password, phone, gender, dob, userType } = req.body;
+    const { fullName, email, password, phone, gender, dob, userType } =
+      req.body;
 
-    if (!fullName || !email || !password || !phone || !gender || !dob) {
+    if (
+      !fullName ||
+      !email ||
+      !password ||
+      !phone ||
+      !gender ||
+      !dob ||
+      !userType
+    ) {
       const error = new Error("All fields Required");
       error.statusCode = 400;
       return next(error);
@@ -25,7 +38,6 @@ export const RegisterUser = async (req, res, next) => {
       url: photoURL,
       publicId: null,
     };
-
     const SALT = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, SALT);
 
@@ -37,13 +49,13 @@ export const RegisterUser = async (req, res, next) => {
       gender,
       dob,
       photo,
-      userType: userType || "customer",
+      userType,
     });
 
     res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
     console.log(error.message);
-    next(error);
+    next();
   }
 };
 
@@ -79,7 +91,7 @@ export const LoginUser = async (req, res, next) => {
     });
   } catch (error) {
     console.log(error.message);
-    next(error);
+    next();
   }
 };
 
@@ -90,6 +102,107 @@ export const LogoutUser = async (req, res, next) => {
     res.status(200).json({ message: "Logout Sucessfully" });
   } catch (error) {
     console.log(error.message);
-    next(error);
+    next();
+  }
+};
+
+export const SendOtp = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      const error = new Error("Email is required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email not registered");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    // Generate and send OTP here
+    const newOTP = (Math.floor(Math.random() * 1000000) + 100000)
+      .toString()
+      .slice(0, 6);
+
+    //Send OTP via Email
+    const hashedOTP = await bcrypt.hash(newOTP, 10);
+    const existingOTP = await OTP.findOne({ email });
+    if (existingOTP) {
+      await existingOTP.deleteOne();
+    }
+
+    const saveOTP = await OTP.create({
+      email,
+      otp: hashedOTP,
+    });
+    await sendOTPEmail(email, newOTP);
+
+    res.status(200).json({ message: `OTP sent on '${email}'` });
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
+export const VerifyOtp = async (req, res, next) => {
+  try {
+    const { email, otp } = req.body;
+
+    if (!email) {
+      const error = new Error("Email is required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingOTP = await OTP.findOne({ email });
+    if (!existingOTP) {
+      const error = new Error("OTP Expired");
+      const statusCode = 401;
+      return next(error);
+    }
+
+    const isVerified = await bcrypt.compare(otp, existingOTP.otp);
+    if (!isVerified) {
+      const error = new Error("OTP Expired");
+      const statusCode = 401;
+      return next(error);
+    }
+
+    await existingOTP.deleteOne();
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email not registered");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    await genOTPToken(existingUser, res);
+    res
+      .status(200)
+      .json({ message: "OTP verified. Create You New Password Now" });
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
+export const ResetPassword = async (req, res, next) => {
+  try {
+    const { newPassword } = req.body;
+
+    const currentUser = req.user;
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    currentUser.password = hashedPassword;
+
+    await currentUser.save();
+
+    res.status(200).json({ message: "Password Changed" });
+  } catch (error) {
+    console.log(error.message);
+    next();
   }
 };
