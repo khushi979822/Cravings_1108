475995d8afad6397d877cf58375dcfb0c036d3a9
khushi979822 <khushikumari5114@gmail.com>
2026-07-13 15:45:13 +0530

    fix: update import paths for routers and controllers; improve error handling in auth and restaurant controllers

--- server/src/routers/restaurant.route.js
+++ server/src/routers/restaurant.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 import { restaurantUpdateProfile } from "../controllers/restaurant.controller.js";
-import { RestaurantAuthProtect } from "../middleware/auth.middelware.js";
+import { RestaurantAuthProtect } from "../middleware/auth.middleware.js";
 
 const upload = multer();
 const router = express.Router();
